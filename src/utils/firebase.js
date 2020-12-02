d351c19ef736de3f85d278d4aafc9f7a9d1bc92a
LIMADAVID <davidlima.dev@gmail.com>
2020-12-01 22:01:06 -0300

    Finalização projeto Birthday

---
+++ src/utils/firebase.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/app'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    //Configuração firebase
+  }
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig)
